8be5509f893003e10cf826223c2a9ce395e86611
Esraa Ragab <esraa.kamel.abdulaziz@gmaill.com>
2026-08-04 03:45:28 -0400

    Creat a new node js app

---
+++ .github/workflows/src/test.sh
@@ -0,0 +1,9 @@
+EXPECTED="Hello, Test!"
+OUTPUT=$(node -e "console.log(require('./src/app')('Test'))")
+if ["$OUTPUT"=="$EXPECTED"]; then
+echo "TEST PASSED"
+exit 0
+else
+echo "TEST FAILED! Expected '$EXPECTED' but got '$OUTPUT'"
+exit 1
+fi
