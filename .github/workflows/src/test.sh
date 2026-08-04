5fd474ee854101a00052bfbc95ff8b9ce2eded1a
Esraa Ragab <esraa.kamel.abdulaziz@gmaill.com>
2026-08-04 04:02:13 -0400

    Add !

--- .github/workflows/src/test.sh
+++ .github/workflows/src/test.sh
@@ -1,9 +1,12 @@
+#!/bin/bash
+
 EXPECTED="Hello, Test!"
 OUTPUT=$(node -e "console.log(require('./src/app')('Test'))")
-if ["$OUTPUT"=="$EXPECTED"]; then
-echo "TEST PASSED"
-exit 0
+
+if [ "$OUTPUT" = "$EXPECTED" ]; then
+    echo "TEST PASSED"
+    exit 0
 else
-echo "TEST FAILED! Expected '$EXPECTED' but got '$OUTPUT'"
-exit 1
+    echo "TEST FAILED! Expected '$EXPECTED' but got '$OUTPUT'"
+    exit 1
 fi
